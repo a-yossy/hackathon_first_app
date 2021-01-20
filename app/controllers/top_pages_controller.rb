73e210bca1b4bb375544141c2948dcff3f326c77
atsuya02 <atsuya0930@yahoo.co.jp>
2021-01-21 01:31:52 +0900

    user sessionの追加

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -1,4 +1,17 @@
 class TopPagesController < ApplicationController
   def show
+    if session[:user]
+      @user = session[:user]
+    end
+  end
+
+  def create
+    session[:user] = params[:session][:user]
+    redirect_to root_url
+  end
+
+  def destroy
+    session.delete(:user)
+    redirect_to root_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'top_pages#show'
+  get    '/login',  to: 'top_pages#show'
+  post   '/login',  to: 'top_pages#create'
+  delete '/logout', to: 'top_pages#destroy'
   namespace :api do
     resources :alarms, only: %i[create]
   end
